fc17ab94763788f4ed2ee0bbb1f7d5ee0f7f8a28
junaid <pjuned.it@gmail.com>
2023-12-30 19:45:54 +0530

    creating instances

--- roboshop.sh
+++ roboshop.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-007049cd4bbc2dbd8
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping"
-"payment" "dispatch" "web") 
+SG_ID=sg-007049cd4bbc2dbd8 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z01102052NPHFMW3XL4SB
+ # replace your zone ID
+DOMAIN_NAME="devopsju.online"
 
 for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
 
-do 
-    echo "Instance is :  $i"
-       if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-       then 
-            INSTANCE_TYPE="t3.small"
-        else
-            INSTANCE_TYPE="t2.micro"
-       fi
-    aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type $INSTANCE_TYPE --security-group-ids sg-007049cd4bbc2dbd8
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-007049cd4bbc2dbd8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done
-
-
