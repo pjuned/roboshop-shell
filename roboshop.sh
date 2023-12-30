55973ea79e8fc660305338039be19f1f2496a014
junaid <pjuned.it@gmail.com>
2023-12-30 19:32:38 +0530

    creating instances

--- roboshop.sh
+++ roboshop.sh
@@ -8,17 +8,14 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipp
 for i in "${INSTANCES[@]}"
 
 do 
-
-echo "Instance is :  $i"
-if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
- then 
-    INSTANCE_TYPE="t3.small"
- else
-    INSTANCE_TYPE="t2.micro"
-fi
-
-aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type $INSTANCE_TYPE --security-group-ids sg-007049cd4bbc2dbd8
-
+    echo "Instance is :  $i"
+       if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+       then 
+            INSTANCE_TYPE="t3.small"
+        else
+            INSTANCE_TYPE="t2.micro"
+       fi
+    aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type $INSTANCE_TYPE --security-group-ids sg-007049cd4bbc2dbd8
 done
 
 
